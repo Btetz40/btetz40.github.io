6b47fff0fb0bb033f935c8dac56a07b07d21fa30
Brendan <btetz@cocc.edu>
2018-10-25 17:06:31 -0700

    .

--- sketckes/empty-example/sketch2.js
+++ sketckes/empty-example/sketch2.js
@@ -1,14 +1,13 @@
 function setup() {
-createCanvas(1000, 1000);
+createCanvas(3000, 1000);
     noStroke();
 }
 function draw() {
-    background(0, 255, 0);
-    
+}
 if (mouseIsPressed) {
-fill(225, 0, 0);
-} else {
-fill(115);
+  fill(225, 0, 0);
+}else {
+  fill(115);
 }
 ellipse(mouseX, mouseY, 40, 40);
-}
+

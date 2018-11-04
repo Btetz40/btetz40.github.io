c179547857401e64880826f6355c37b54391676d
Brendan <btetz@cocc.edu>
2018-11-03 22:52:20 -0800

    Changed some shit

--- sketckes/empty-example/sketch2.js
+++ sketckes/empty-example/sketch2.js
@@ -1,11 +1,11 @@
 function setup() {
-    createCanvas(480, 120);
+    createCanvas(1000, 1200);
     }
     function draw() {
     if (mouseIsPressed) {
     fill(0);
     } else {
-    fill(255);
+    fill(175);
     }
-    ellipse(mouseX, mouseY, 80, 80);
+    ellipse(mouseX, 80);
     }
